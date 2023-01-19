2a9c4a1ef91eeae9ce51a890c5f97b1ffbe00110
zhihaozhang <zhihaozhang98@outlook.com>
2023-01-19 16:52:25 +0800

    add : dal init

---
+++ TIKTOK_User/dal/init.go
@@ -0,0 +1,7 @@
+package dal
+
+import "GoProject/dal/mysql"
+
+func Init() {
+	mysql.Init()
+}
---
+++ TIKTOK_User/dal/mysql/init.go
@@ -0,0 +1,34 @@
+package mysql
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+	"time"
+)
+
+var dsn = "root1:root1234@tcp(120.25.2.146:3306)/tiktok?charset=utf8&parseTime=True&loc=Local"
+
+var DB *gorm.DB
+
+func Init() {
+	// gorm日志打印
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,   // 慢 SQL 阈值
+			LogLevel:      logger.Silent, // 日志级别
+			Colorful:      true,          // 彩色打印
+		},
+	)
+
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+	if err != nil {
+		panic(err)
+	}
+}
