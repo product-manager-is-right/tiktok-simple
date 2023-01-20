28803157e6d871f3ebe62b37ac5482ec58cf60e3
zhihaozhang <zhihaozhang98@outlook.com>
2023-01-20 20:21:53 +0800

    Add : route,jwt middleware

--- TIKTOK_User/api/router/route.go
+++ TIKTOK_User/api/router/route.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"GoProject/api/handler"
+	"GoProject/mw"
 	"github.com/cloudwego/hertz/pkg/app/server"
 )
 
@@ -10,22 +11,31 @@ import (
 func GeneratedRegister(r *server.Hertz) {
 	// user路由组
 	userRouter := r.Group("/douyin/user")
-	userRouter.GET("/", handler.UserInfo)
-	userRouter.POST("/register/", handler.Register)
-	userRouter.POST("/login/", handler.Login)
+	userRouter.GET("/", mw.JwtMiddleware.MiddlewareFunc(), handler.UserInfo)
+	userRouter.POST("/register/", handler.Register, mw.JwtMiddleware.LoginHandler)
+	userRouter.POST("/login/", mw.JwtMiddleware.LoginHandler)
 
 	// publish路由组
 	publishRouter := r.Group("/douyin/publish")
+
+	publishRouter.Use(mw.JwtMiddleware.MiddlewareFunc())
+
 	publishRouter.POST("/action/", handler.PublishAction)
 	publishRouter.GET("/list/", handler.PublishList)
 
 	// favorite路由组
 	favoriteRouter := r.Group("/douyin/favorite")
+
+	favoriteRouter.Use(mw.JwtMiddleware.MiddlewareFunc())
+
 	favoriteRouter.POST("/action/", handler.FavoriteAction)
 	favoriteRouter.GET("/list/", handler.FavoriteList)
 
 	// relation路由组
 	relationRouter := r.Group("/douyin/relation")
+
+	relationRouter.Use(mw.JwtMiddleware.MiddlewareFunc())
+
 	relationRouter.POST("/action/", handler.RelationAction)
 	relationRouter.GET("/follow/list/", handler.FollowList)
 	relationRouter.GET("/follower/list/", handler.FollowerList)
