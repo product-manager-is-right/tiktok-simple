d13bf475121c10ffd459ee749a1f5ae699cdf3a3
smx-Morgan <1048492656@qq.com>
2023-01-21 00:04:14 +1300

    finish the class for Video Part

--- TIKTOK_Video/cmd/main.go
+++ TIKTOK_Video/cmd/main.go
@@ -1,1 +1,23 @@
 package cmd
+
+import (
+	"TIKTOK_Video/api/router"
+	"TIKTOK_Video/dal"
+	"github.com/cloudwego/hertz/pkg/app/server"
+)
+
+func main() {
+	initDeps()
+
+	r := server.Default()
+
+	// 注册路由
+	router.GeneratedRegister(r)
+
+	r.Spin()
+}
+
+func initDeps() {
+	// 初始化数据库
+	dal.Init()
+}
--- TIKTOK_Video/dal/init.go
+++ TIKTOK_Video/dal/init.go
@@ -1,6 +1,6 @@
 package dal
 
-import "TIKTOK_Video/TIKTOK_Video/dal/mysql"
+import "TIKTOK_Video/dal/mysql"
 
 func Init() {
 	mysql.Init()
