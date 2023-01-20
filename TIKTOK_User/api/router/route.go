50a17568c8bb9093132458a5b577420086fd196b
zhihaozhang <zhihaozhang98@outlook.com>
2023-01-20 14:14:37 +0800

    Update : add all router

--- TIKTOK_User/api/router/route.go
+++ TIKTOK_User/api/router/route.go
@@ -8,9 +8,26 @@ import (
 // GeneratedRegister
 // 注册路由
 func GeneratedRegister(r *server.Hertz) {
-	apiRouter := r.Group("/douyin/user")
+	// user路由组
+	userRouter := r.Group("/douyin/user")
+	userRouter.GET("/", handler.UserInfo)
+	userRouter.POST("/register/", handler.Register)
+	userRouter.POST("/login/", handler.Login)
 
-	apiRouter.GET("/", handler.UserInfo)
-	apiRouter.POST("/register/", handler.Register)
-	apiRouter.POST("/login/", handler.Login)
+	// publish路由组
+	publishRouter := r.Group("/douyin/publish")
+	publishRouter.POST("/action/", handler.PublishAction)
+	publishRouter.GET("/list/", handler.PublishList)
+
+	// favorite路由组
+	favoriteRouter := r.Group("/douyin/favorite")
+	favoriteRouter.POST("/action/", handler.FavoriteAction)
+	favoriteRouter.GET("/list/", handler.FavoriteList)
+
+	// relation路由组
+	relationRouter := r.Group("/douyin/relation")
+	relationRouter.POST("/action/", handler.RelationAction)
+	relationRouter.GET("/follow/list/", handler.FollowList)
+	relationRouter.GET("/follower/list/", handler.FollowerList)
+	relationRouter.GET("/friend/list/", handler.FriendList)
 }
