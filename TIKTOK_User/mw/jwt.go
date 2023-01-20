d4ca4e9b82536bc0c835415ec8fc00852e7aaf57
zhihaozhang <zhihaozhang98@outlook.com>
2023-01-20 20:24:41 +0800

    add : jwt init

---
+++ TIKTOK_User/mw/jwt.go
@@ -0,0 +1,87 @@
+package mw
+
+import (
+	"GoProject/dal/mysql"
+	"GoProject/model"
+	"GoProject/util"
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/common/hlog"
+	"github.com/cloudwego/hertz/pkg/common/utils"
+	"github.com/hertz-contrib/jwt"
+)
+
+var (
+	JwtMiddleware *jwt.HertzJWTMiddleware
+	IdentityKey   = "user_name"
+)
+
+func InitJwt() {
+	var err error
+	JwtMiddleware, err = jwt.New(&jwt.HertzJWTMiddleware{
+		Realm:       "tiktok",
+		Key:         []byte("jwt sign key"),
+		Timeout:     time.Hour,
+		MaxRefresh:  time.Hour,
+		TokenLookup: "query: token",
+		Authenticator: func(ctx context.Context, c *app.RequestContext) (interface{}, error) {
+			var loginStruct struct {
+				UserName string `form:"username" json:"username" query:"username" `
+				Password string `form:"password" json:"password" query:"password"`
+			}
+			if err := c.BindAndValidate(&loginStruct); err != nil {
+				return nil, err
+			}
+			users, err := mysql.CheckUser(loginStruct.UserName, util.MD5(loginStruct.Password))
+			if err != nil {
+				return nil, err
+			}
+			if len(users) == 0 {
+				return nil, errors.New("user already exists or wrong password")
+			}
+			c.Set("user_id", users[0].Id)
+			return users[0], nil
+		},
+		LoginResponse: func(ctx context.Context, c *app.RequestContext, code int, token string, expire time.Time) {
+			c.JSON(http.StatusOK, utils.H{
+				"status_code": 1,
+				"status_mgs":  "login success",
+				"user_id":     c.Value("user_id").(int64),
+				"token":       token,
+			})
+		},
+		IdentityKey: IdentityKey,
+		IdentityHandler: func(ctx context.Context, c *app.RequestContext) interface{} {
+			claims := jwt.ExtractClaims(ctx, c)
+			return &model.User{
+				Name: claims[IdentityKey].(string),
+			}
+		},
+		PayloadFunc: func(data interface{}) jwt.MapClaims {
+			if v, ok := data.(*model.User); ok {
+				return jwt.MapClaims{
+					IdentityKey: v.Name,
+				}
+			}
+			return jwt.MapClaims{}
+		},
+		HTTPStatusMessageFunc: func(e error, ctx context.Context, c *app.RequestContext) string {
+			hlog.CtxErrorf(ctx, "jwt err = %+v", e.Error())
+			return e.Error()
+		},
+		Unauthorized: func(ctx context.Context, c *app.RequestContext, code int, message string) {
+			c.JSON(http.StatusOK, utils.H{
+				"status_code": 1,
+				"status_msg":  "jwt authorize fail",
+			})
+		},
+	})
+	if err != nil {
+		log.Fatal("jwt init fail")
+	}
+}
