78fb4e8378323f1eebc3cebc617a6b88a961280b
zhihaozhang <zhihaozhang98@outlook.com>
2023-01-20 20:22:48 +0800

    add : md5

--- TIKTOK_User/util/util.go
+++ TIKTOK_User/util/util.go
@@ -1,10 +1,13 @@
 package util
 
-//ValidateToken
-/*
-验证token是否通过
-*/
-func ValidateToken(token string) bool {
-	// TODO : impl
-	return true
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
+// MD5 use md5 to encrypt strings
+func MD5(str string) string {
+	h := md5.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
 }
