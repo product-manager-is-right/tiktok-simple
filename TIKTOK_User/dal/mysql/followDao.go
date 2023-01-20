98fd28fdabea0d8f03db98558dd5f04c99bf801f
zhihaozhang <zhihaozhang98@outlook.com>
2023-01-20 13:46:50 +0800

    add : Dao layer

---
+++ TIKTOK_User/dal/mysql/followDao.go
@@ -0,0 +1,19 @@
+package mysql
+
+/*
+GetFollowCntByUserId
+根据UserId查询该用户的关注数
+*/
+func GetFollowCntByUserId(userId int64) (int64, error) {
+	// TODO : impl
+	return 0, nil
+}
+
+/*
+GetFollowerCntByUserId
+根据UserId查询该用户的粉丝数/被关注数
+*/
+func GetFollowerCntByUserId(userId int64) (int64, error) {
+	// TODO : impl
+	return 0, nil
+}
---
+++ TIKTOK_User/dal/mysql/userDao.go
@@ -0,0 +1,8 @@
+package mysql
+
+import "GoProject/model"
+
+func GetUserByUserId(userId int64) (model.User, error) {
+	// TODO : impl
+	return model.User{}, nil
+}
