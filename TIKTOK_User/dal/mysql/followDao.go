34929bea02b1c19b808288212c149c13eb3a30a9
smx-Morgan <1048492656@qq.com>
2023-01-27 18:06:08 +0800

    bugFixed

--- TIKTOK_User/dal/mysql/followDao.go
+++ TIKTOK_User/dal/mysql/followDao.go
@@ -1,12 +1,30 @@
 package mysql
 
+import (
+	"GoProject/model"
+	"log"
+)
+
 /*
 GetFollowCntByUserId
 根据UserId查询该用户的关注数
 */
 func GetFollowCntByUserId(userId int64) (int64, error) {
 	// TODO : impl
-	return 1, nil
+	//return 1, nil
+	var cnt int64 = 10
+	// 当查询出现错误的情况，日志打印err msg，并返回err.
+	if err := DB.
+		Model(model.Follow{}).
+		Where("user_id_from = ?", userId).
+		Where("cancel = ?", 0).
+		Count(&cnt).Error; nil != err {
+		log.Println(err.Error())
+		return 0, err
+	}
+	// 正常情况，返回取到的粉丝数。
+	log.Printf("有%d个粉丝", cnt)
+	return cnt, nil
 }
 
 /*
@@ -15,7 +33,20 @@ GetFollowerCntByUserId
 */
 func GetFollowerCntByUserId(userId int64) (int64, error) {
 	// TODO : impl
-	return 1, nil
+	// 用于存储当前用户粉丝数的变量
+	var cnt int64
+	// 当查询出现错误的情况，日志打印err msg，并返回err.
+	if err := DB.
+		Model(model.Follow{}).
+		Where("user_id_to = ?", userId).
+		Where("cancel = ?", 0).
+		Count(&cnt).Error; nil != err {
+		log.Println(err.Error())
+		log.Printf("有%d个关注", cnt)
+		return 0, err
+	}
+	// 正常情况，返回取到的粉丝数。
+	return cnt, nil
 }
 
 /*
@@ -24,5 +55,25 @@ GetIsFollow
 */
 func GetIsFollow(userIdDst, userIdSrc int64) (bool, error) {
 	// TODO : impl
-	return false, nil
+	// 用于存储查出来的关注关系。
+	follow := model.Follow{}
+
+	//notIsNull := false
+	//当查询出现错误时，日志打印err msg，并return err.
+	if err := DB.
+		Where("user_id_from = ?", userIdSrc).
+		Where("user_id_to = ?", userIdDst).
+		Where("cancel = ? or cancel = ?", 0, 1).
+		Take(&follow).Error; nil != err {
+		// 当没查到记录报错时，不当做错误处理。
+		if "record not found" == err.Error() {
+			return false, nil //return nil,nil  只能传参为(*Follow, error)
+
+		}
+		log.Println(err.Error())
+		return false, err //return nil,err 只能传参为([]int64, error)
+	}
+	//正常情况，返回取到的关系和空err.
+	//return &follow, nil  r如果是查询对应的关注关系，直接返回 &follow，前提是传参为([]int64, error)
+	return true, nil
 }
--- TIKTOK_User/test/1_test.go
+++ TIKTOK_User/test/1_test.go
@@ -5,16 +5,53 @@ import (
 	"fmt"
 	"github.com/cloudwego/hertz/pkg/common/test/assert"
 	"log"
+	"os"
+	"strconv"
 	"testing"
 )
 
-func TestA(t *testing.T) {
+func TestMain(m *testing.M) {
 	mysql.Init()
+	code := m.Run()
+
+	os.Exit(code)
+}
+func TestA(t *testing.T) {
 	res, err := mysql.GetPublishVideoIdsById(24)
 	if err != nil {
 		log.Print(err)
 		log.Print("find object failed")
 	}
 	fmt.Println(res)
+	assert.Nil(t, nil)
+}
+func TestFollow(t *testing.T) {
+	res, err := mysql.GetFollowCntByUserId(25)
+	fmt.Printf(strconv.FormatInt(res, 10))
+	if err != nil {
+		log.Print(err)
+		log.Print("find object failed")
+	}
+	assert.Nil(t, nil)
+}
+func TestFollower(t *testing.T) {
+	res, err := mysql.GetFollowerCntByUserId(24)
+	fmt.Printf(strconv.FormatInt(res, 10))
+	if err != nil {
+		log.Print(err)
+		log.Print("find object failed")
+	}
+	assert.Nil(t, nil)
+}
+func TestIsFollow(t *testing.T) {
+	res, err := mysql.GetIsFollow(24, 25)
+
+	if err != nil {
+		log.Print(err)
+		log.Print("find object failed")
+	}
+	if res {
+		fmt.Println("successful")
+	}
 	assert.Nil(t, nil)
 }
